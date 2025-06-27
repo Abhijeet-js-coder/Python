b6a9463174889266c2f12b70766498551693610c
Abhijeet Das <abhijeetd887@gmail.com>
2025-06-27 11:26:58 +0530

    Add files via upload

---
+++ formatted_strings.py
@@ -0,0 +1,10 @@
+#ALternate method but not recommended by advance programmers
+first='Vishy '
+last='Anand'
+print(first +  last, "is an Indian Chess Grandmaster")
+
+#Formatted string
+first_name='John'
+last_name='Doe'
+msg= f'{first_name} {last_name}, is a coder' #using f in the starting is important as it adds prefix
+print(msg)
